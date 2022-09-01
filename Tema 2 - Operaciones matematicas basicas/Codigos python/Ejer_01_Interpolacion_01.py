ee3931cdbc0a01977281a67f0b032af7b4e4ee97
guxtux <gus.contreras.m@gmail.com>
2022-09-01 14:29:59 -0500

    Interpolación Lagrange - 1 de septiembre de 2022 - Laptop
    Se avanza en el material de interpolación de Lagrange con el notebook.

--- Tema 2 - Operaciones matematicas basicas/Codigos python/Ejer_01_Interpolacion_01.py
+++ Tema 2 - Operaciones matematicas basicas/Codigos python/Ejer_01_Interpolacion_01.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 10 19:51:36 2020
+# """
+# Created on Tue Mar 10 19:51:36 2020
 
-@author: gusto
-"""
+# @author: gusto
+# """
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
+
+mpl.rcParams['text.usetex'] = True
+
+def etiquetas():
+    plt.title('Ejercicio de interpolación')
+    plt.xlabel('Variable independiente x')
+    plt.ylabel('Variable dependiente P(x)')
+
 n = 3
-x_0_ = np.array([1.5, 2.5, 3.5])
+x0 = np.array([1.5, 2.5, 3.5])
 x = np.array([1., 2., 3., 4.])
 f = np.array([0.671, 0.620, 0.567, 0.512])
 
-for k in x_0_:
+datos = []
+
+for k in x0:
     yres = 0
     for i in range(0, n + 1):
         z = 1.0
@@ -20,4 +32,19 @@ for k in x_0_:
                 z = z * (k - x[j])/(x[i] - x[j])
         yres = yres + z*f[i]
     
+    datos.append(yres)
     print ('El polinomio evaluado en P(',k,') =',  yres)
+   
+plot1 = plt.figure(1)
+plt.plot(x, f, '+r', label='Experimentales')
+etiquetas()
+plt.savefig('Ejercicio_Interpolacion_01.eps', format='eps')
+
+plot2 = plt.figure(2)
+plt.plot(x, f, '+r', label='Experimentales')
+plt.plot(x0, datos, 'ob', label= 'Interpolados')
+plt.legend(loc='best')
+etiquetas()
+plt.savefig('Ejercicio_Interpolacion_02.eps', format='eps')
+
+plt.show()
